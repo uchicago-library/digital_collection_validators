3944eb09bbd5c39bcf521d179881453333460d60
Amanda Wong <wongamanda@UCHICAGO.EDU>
2018-04-16 10:52:57 -0500

    working file

--- proj1.py
+++ proj1.py
@@ -4,7 +4,9 @@ import os
 """ checkRoot evaluates there are 3 subdirs (ALTO, JPEG, TIFF) and 5 files (xml, pdf, txt)
 	checkRoot throws an error if there are not 3 subdirs and 5 files"""
 def checkRoot(r):
-	for n_item in scandir(r):
+	dirNum = 0
+	fileNum = 0
+	for n_item in os.scandir(r):
 		if n_item.is_dir():
 			dirNum += 1
 		elif n_item.is_file():
@@ -14,18 +16,18 @@ def checkRoot(r):
 
 """ fileChoices takes paramaters of extensions (@choices) and a list of child files (@flist). 
 	It traverses fList to see if all files end with choices """
-def fileChoices(fType, fList):
+def fileChoices(fType, fList, d):
 	extDict = {}
 	for f in fList:
 		ext = os.path.splitext(f)[1][1:]
-		print(ext)
 		if ext != fType:
-			parser.error('one of the files does not end with {}'.format(fType))
-		if str(ext) in fList:
+			parser.error('one of the files in {} does not end with {}'.format(d, fType))
+		if ext in extDict:
 			extDict[ext] += 1
 		else:
 			extDict[ext] = 1
-		print(f)
+	return extDict
+	
 
 """ observing """
 parser = argparse.ArgumentParser(description='Checks OCR directory')
@@ -38,27 +40,27 @@ def main():
 	args_dict = vars(args)
 	directoryTree = os.walk(args.directory)
 
+	checkRoot(args.directory)
 	for root, dirs, files in directoryTree:
-		# temporary: draws tree for reference
-		path = root.split(os.sep)
-		print((len(path) - 1) * '---', os.path.basename(root))
-		for f in files:
-			print(len(path) * '---', f)
-
-		# traverses tree
 		for d in dirs:
+			path = os.path.join(root, d)
 			if d == 'ALTO':
-				numAlto = len(os.listdir(d))
-				fileChoices('xml', os.listdir(d))
-			if d == 'JPEG':
-				numJPEG = len(os.listdir(d))
-				fileChoices('jpeg', os.listdir(d))
-			if d == 'TIFF':
-				numTiff = len(os.listdir(d))
-				fileChoices('tif', os.listdir(d))
+				checker = fileChoices('xml', os.listdir(path), d)
+				numAlto = checker['xml']
+			elif d == 'JPEG':
+				checker = fileChoices('jpg', os.listdir(path), d)
+				numJPEG = checker['jpg']
+			elif d == 'TIFF':
+				checker = fileChoices('tif', os.listdir(path), d)
+				numTif = checker['tif']
+			else:
+				parser.error('Remove the folder that is not ALTO, JPEG, or TIFF. Suggestion: it could be the PDF \
+					file that needs removal')
 
-		if numAlto != numJPEG != numTiff:
-			parser.error('the number of files in each subdirectory do not match one another.')
+	if numAlto != numJPEG != numTif:
+		parser.error('The number of files in the subdirectories is inconsistent.')
+	else:
+		print('hooray! you have the proper number of files with the right number of extensions')
 
 	try:
 		return 0
@@ -67,4 +69,11 @@ def main():
 
 if __name__ == '__main__':
 	main()
+
+
+		# temporary: draws tree for reference
+		# path = root.split(os.sep)
+		# print((len(path) - 1) * '---', os.path.basename(root))
+		# for f in files:
+		# 	print(len(path) * '---', f)
 
