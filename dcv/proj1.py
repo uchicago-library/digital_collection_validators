3bf03f485c9d2df5203e2e4cc8fdf3bd086754be
Amanda Wong <wongamanda@UCHICAGO.EDU>
2018-05-18 11:55:49 -0500

    unit testing

--- dcv/proj1.py
+++ dcv/proj1.py
@@ -1,5 +1,6 @@
 import os
 import time
+import sys
 import argparse
 from os import _exit
 
@@ -7,90 +8,48 @@ errorLogs = []
 
 def checkRoot(r): 
 	"""Determines the proper number of subdirectories and files.
-	
 	Checks for 3 subdirs (ALTO, JPEG, TIFF) and 5 files (xml, pdf, txt) at the top-level of the input directory.
-	
 	:param generator r: top-level directory of input
 	:rtype None 
 	"""
+	rChoices = ['dc.xml', 'struct.txt', 'mets.xml', 'txt', 'pdf']
 	dirNum, fileNum = 0, 0
 	rIter = os.scandir(r)
 	rList = []
 	mvols = set()
-	try:
-		while True:
-			elem = next(rIter)
-			if elem.is_dir():
-				dirNum += 1
-			elif elem.is_file():
-				fileNum += 1
-				rList.append(elem)
-	except StopIteration:
-		pass
 
-	extDict = fileChoices(['dc.xml', 'struct.txt', 'mets.xml', 'txt', 'pdf'], rList, os.path.basename(r))
-	if not all(value == 1 for value in extDict.values()):
-		errorLogs.append('There is a missing file type from the root directory. Please refer to the Campus Pub \
-			spec sheet for the required OCR data formats.')	
-	elif dirNum != 3 or fileNum != 5:
-		errorLogs.append('This directory does not have 3 subdirectories and 5 files.')
+	for elem in rIter:
+		if elem.is_dir():
+			dirNum += 1
+		elif elem.is_file():
+			fileNum += 1
+			rList.append(elem)
+			mvols.add(os.path.basename(elem).split('.', 1)[0])
 
-	for m in rList:
-		mvols.add(os.path.basename(m).split('.', 1)[0])
+	extensionDict = fileChoices(rChoices, rList)
 	if len(mvols) != 1:
 		errorLogs.append('The mvol identifiers of files in the root directory require uniformity.')
+	if dirNum != 3 or fileNum != 5:
+		errorLogs.append('The root directory does not have 3 subdirectories and 5 files.')
+	elif not all(value == 1 for value in extensionDict.values()):
+		errorLogs.append('There is a missing file type from the root directory. Please refer ' \
+			'to the Campus Pub spec sheet for the required OCR data formats.')
 
-def mvolIdentifier(pt, subdirName):
-	"""Evaluates whether 1) all mvol identifiers match and 2) their pagination is consistent
-	
-	Selects the identifier and ensures they all match.
-
-	Appends last 4 digits of each mvol and ensures that there aren't any missing pages 
-	in a recursive function checkPagination. 
-
-	func checkPagination confirms the subdirectory's pagination monotonically increases or decreases.
-
-	:param list subdirList: an os.listdir of strings
-	:param str subdirName: name of subdirectory
-	:rtype None
-	""" 
-	subdirList = os.scandir(pt)
-	fullMvol = []
-	pagination = []
-
-	for s in subdirList:
-		if s.is_file():
-			fullMvol.append(os.path.basename(s).split('_', 1)[0])
-			pagination.append(os.path.basename(s).split('_', 1)[1][:4])
-
-	def checkPagination(pList, d):
-		try:
-			if len(pList) == 1:
-				return True
-			if abs(int(pList[-1]) - int(pList[-2])) != 1:
-				errorLogs.append('The pagination does not increase or decrease by 1 in {}.'.format(d))
-			checkPagination(pList[:-1], d)
-		except IndexError:
-			raise ValueError('***Attention***: Please remove the subdirectory in {}.'.format(d))
-
-	if len(set(fullMvol)) != 1:
-		errorLogs.append('An mvol identifier in folder {} is inconsistent with the rest.'.format(subdirName))
-
-	checkPagination(pagination, subdirName)
-
-def fileChoices(fType, fList, d):
+def fileChoices(fType, d):
 	"""Traverses a list of files and evaluates whether they all match a file extension.
-	
-	:param array fType: a list of string of possible extensions a file can be, depending on its directory.
-	:param arr fList: a list of child files in a directory.
-	:param [directory]
+	:param array fType: a list of expected file extensions (str)
+	:param generator d: directory of files
+	:rtype: None
 	"""
+	if isinstance(d, (list,)):
+		fList = d
+		d = 'root'
+	else:
+		fList = os.scandir(d)
 	extDict = {}
-
 	for f in fList:
 		if f.is_dir():
-			errorLogs.append('There is a directory within subdirectory {}. Please remove it ' \
-				'so testing can resume.'.format(d))
+			errorLogs.append('Please remove the nested directory "{}" in {}.'.format(os.path.basename(f), d))
 		else:
 			ext = os.path.basename(f).split('.', 1)[1:][0]
 			if ext not in fType:
@@ -99,62 +58,95 @@ def fileChoices(fType, fList, d):
 				extDict[ext] += 1
 			else:
 				extDict[ext] = 1
-
+	print(extDict)
 	return extDict
-	
-def main():
+
+def mvolIdentifier(r):
+	"""Evaluates whether 1) all mvol identifiers match and 2) their pagination is consistent
+	func checkPagination confirms the subdirectory's pagination monotonically increases or decreases.
+	:param generator r: directory of files
+	:rtype: None
+	""" 
+	subdirList = os.scandir(r)
+	fullMvol = []
+	pagination = []
+
+	for s in subdirList:
+		if s.is_file():
+			fullMvol.append(os.path.basename(s).split('_', 1)[0])
+			pagination.append(os.path.basename(s).split('_', 1)[1][:4])
+
+	if len(set(fullMvol)) != 1:
+		errorLogs.append('An mvol identifier in folder {} is inconsistent with the rest.'.format(r))
+
+	def checkPagination(pList, d):
+		if len(pList) == 1:
+			return True
+		if abs(int(pList[-1]) - int(pList[-2])) != 1:
+			errorLogs.append('The pagination does not increase or decrease by 1 in {}.'.format(d))
+		checkPagination(pList[:-1], d)
+	checkPagination(pagination, r)
+
+
+def parseArgs(args):
+	# Builds argparse parser to receive input
 	parser = argparse.ArgumentParser(description='Checks OCR directory')
 	parser.add_argument(
-		'-dir', '--directory', action='store', required=True, \
-		default='/tmp/non_existent_dir', help='Input OCR data directory path')
-	args = parser.parse_args()
+		'-dir', '--directory', action='store', required=True, default=os.getcwd(), help='Input OCR data directory path')
+	return parser.parse_args(args)	
 
-	if os.path.isdir(args.directory):
+def main():
+	parse = parseArgs(sys.argv[1:])
+	if os.path.isdir(parse.directory):
 		pass
 	else:
-		parser.error('{} is an invalid directory. Please input an existing directory.'.format(args.directory))
+		parser.error('{} is an invalid directory. Please input an existing directory.'.format(parse.directory))
 
 	try:
-		directoryTree = os.walk(args.directory)
-		checkRoot(args.directory)
+		directoryTree = os.walk(parse.directory)
+		acceptableDirs = [
+			{'name': 'ALTO', 'extension': 'xml'},
+			{'name': 'JPEG', 'extension': 'jpg'},
+			{'name': 'TIFF', 'extension': 'tif'},
+			{'name': '0108', 'extension': False}
+		]
 
-		treetime = directoryTree.__next__()
-		root = treetime[0]
-		dirs = treetime[1]
-		fils = treetime[2]
+		def checkDirs(r, dList, pName):
+			"""Traverses a directory, counts its files, and calls functions on files
+			:param generator r: directory
+			:param list dList: list of expected extension objects, acceptableDirs
+			:param str pName: name of directory
+			"""
 
-		print('here is the root', root)
-		print('here are the dirs', dirs)
-		print('here are the fils', fils)
-		print(len(treetime))
+			if len(dList) == 1 and dList[0]['name'] != pName:
+				errorLogs.append('Please remove or rename the folder that is not ALTO, JPEG, or TIFF. ')
+				return
+			cur = dList[len(dList) -1]
+			if pName == '0108':
+				checkRoot(r)
+				return
+			elif cur['name'] == pName:
+				ext = cur['extension']
+				cur.update({'num': fileChoices([ext], r)[ext]})
+				mvolIdentifier(r)
+				return
+			checkDirs(r, dList[:-1], pName)
 
-		# for root, dirs, files in directoryTree:
-		# 	for d in dirs:
-		# 		path = os.path.join(root, d)
-		# 		scanPath = os.scandir(path)
-		# 		if d == 'ALTO':
-		# 			numAlto = fileChoices(['xml'], scanPath, d)['xml']
-		# 			mvolIdentifier(path, d)
-		# 		elif d == 'JPEG':
-		# 			numJPEG = fileChoices(['jpg'], scanPath, d)['jpg']
-		# 			mvolIdentifier(path, d)
-		# 		elif d == 'TIFF':
-		# 			numTif = fileChoices(['tif'], scanPath, d)['tif']
-		# 			mvolIdentifier(path, d)
-		# 		else:
-		# 			errorLogs.append('Please remove or rename the folder that is not ALTO, JPEG, or TIFF. \
-		# 				Note: PDF should not have a folder.')
-		# if (numAlto * 3) != numAlto + numJPEG + numTif:
-		# 	errorLogs.append('The number of files in the subdirectories is inconsistent.')
+		for root, dirs, files in directoryTree:
+			pathName = root[-4:].upper()
+			checkDirs(root, acceptableDirs, pathName)				
 
-		localTime = time.localtime()
-		currentTime = time.strftime('%I:%M%p on %b %d, %Y', localTime)
+		if (acceptableDirs[0]['num'] * 3) != sum([(i['num']) for i in acceptableDirs if i['extension']]):
+			errorLogs.append('The number of files in the subdirectories is inconsistent. Please ' \
+				'remove the missing or extra file.')
+
+		currentTime = time.strftime('%I:%M%p on %b %d, %Y', time.localtime())
 		if len(errorLogs) == 0:
-			print(' -------------- RESULTS FROM VALIDATOR TOOL -------------- ')
+			print(' ============== RESULTS FROM VALIDATOR TOOL ============== ')
 			print('            Test ran', currentTime, '          ')
-			print('               OCR data passed all tests!')
+			print('                OCR data passed all tests!')
 		else:
-			print(' -------------- RESULTS FROM VALIDATOR TOOL -------------- ')
+			print(' ============== RESULTS FROM VALIDATOR TOOL ============== ')
 			print('            Test ran', currentTime, '          ')
 			print('\n'.join([error for error in errorLogs]))
 		return 0
@@ -166,25 +158,3 @@ def main():
 if __name__ == '__main__':
 	_exit(main())
 
-
-''' ALTERNATIVE FOR OOP VERSION '''
-
-# import argparse
-
-# class MyClass(object):
-#   def __init__(self, foo, bar):
-#     self.foo = foo
-#     self.bar = bar
-
-#   def Print(self):
-#     print self.foo
-#     print self.bar
-
-# def main():
-#   parser = argparse.ArgumentParser()
-#   parser.add_argument('foo')
-#   parser.add_argument('bar')
-#   args = parser.parse_args()
-#   c1 = MyClass(args.foo, args.bar)
-#   args_dict = vars(args)
-#   c2 = MyClass(**args_dict)
--- tests/tests.py
+++ tests/tests.py
@@ -1,54 +1,70 @@
 import os
 import unittest
+import argparse
 import tempfile
+import shutil
+from dcv.proj1 import main, parseArgs, fileChoices, checkRoot, mvolIdentifier
 
+# def main():
+# 	sysDir = tempfile.gettempdir()
+# 	testDir = os.path.join(sysDir, 'testdir')
+# 	testDir = tempfile.mkdtemp(suffix=None, prefix=None, dir=None)
+# 	altoDir = tempfile.mkdtemp(suffix=None, prefix='ALTO', dir= testDir)
+# 	jpgDir = tempfile.mkdtemp(suffix=None, prefix='JPEG', dir= testDir)
+# 	tifDir = tempfile.mkdtemp(suffix=None, prefix='TIFF', dir= testDir)
 
-# @mock.patch('proj1.listdir')
-# class testPatch(self):
-# 	def test_checkRoot():
-# 		with patch('os.path.isfile', lambda x: True):
-# 			self.assertTrue(checkRoot(self))
+# 	tempfile.mkstemp(suffix='.xml', prefix='mvol-0004-1920-0108_0001', dir=altoDir)
+# 	tempfile.mkstemp(suffix='.jpg', prefix='mvol-0004-1920-0108_0001', dir=jpgDir)
+# 	tempfile.mkstemp(suffix='.tif', prefix='mvol-0004-1920-0108_0001', dir=tifDir)
 
-# creating a fixture
-# pytestmark = pytest.mark.usefixtures('cleanDir')
-
-subdirFiles = ['mvol-0004-1920-0320_0001.xml', 'mvol-0004-1920-0320_0002.xml', 'mvol-0004-1920-0320_0003.xml']
-
-
+# 	def list_files(startpath):
+# 		for root, dirs, files in os.walk(startpath):
+# 			level = root.replace(startpath, '').count(os.sep)
+# 			indent = ' ' * 4 * (level)
+# 			print('{}{}/'.format(indent, os.path.basename(root)))
+# 			subindent = ' ' * 4 * (level + 1)
+# 			for f in files:
+# 				print('{}{}'.format(subindent, f))
+# 	list_files(testDir)
+	  
 
 class projTests(unittest.TestCase):
+
 	def setUp(self):
-		newPath = tempfile.mkdtemp()
-		os.chdir(newPath)
+		'''
+		Create a temporary dir, add 3 subdirs, 5 files.
+		Within subdirs, add 2 files (different extensions)
+		'''
+		sysDir = tempfile.gettempdir()
+		testDir = os.path.join(sysDir, 'testdir')
+		self.mainDir = tempfile.mkdtemp(suffix=None, prefix=None, dir=None)
+		self.aDir = tempfile.mkdtemp(suffix=None, prefix='ALTO', dir= self.mainDir)
+		self.jDir = tempfile.mkdtemp(suffix=None, prefix='JPEG', dir= self.mainDir)
+		self.tDir = tempfile.mkdtemp(suffix=None, prefix='TIFF', dir= self.mainDir)
 
-	def test_checkRoot(self):
+		tempfile.mkstemp(suffix='.xml', prefix='mvol-0004-1920-0108_0001', dir=self.aDir)
+		tempfile.mkstemp(suffix='.xml', prefix='mvol-0004-1920-0108_0002', dir=self.aDir)		
+		tempfile.mkstemp(suffix='.jpg', prefix='mvol-0004-1920-0108_0001', dir=self.jDir)
+		tempfile.mkstemp(suffix='.jpg', prefix='mvol-0004-1920-0108_0002', dir=self.jDir)
+		tempfile.mkstemp(suffix='.tif', prefix='mvol-0004-1920-0108_0001', dir=self.tDir)
+		tempfile.mkstemp(suffix='.tif', prefix='mvol-0004-1920-0108_0002', dir=self.tDir)
 
+	# def test_main(self):
+	# 	self.assertEqual(main(), 'ok')
+	# 	self.assertEqual(main(), )
 
-	# MVOLIDENTIFIER raises Error
-	def test_mvolIdentifier(self):
-		test_object = subdirFiles
-		test_
-
-
-	# FILECHOICES raises Error
 	def test_fileChoices(self):
-
-
-	# checks whether directory exists
-
-
-# 	def runTest(self):
-# 		self.assertEqual(self, )
-
-
+		result = fileChoices(['.xml', '.dc.xml', '.pdf', '.struct.text', '.text'], self.mainDir)
+		# assert result is {'dc.xml': 1, 'mets.xml': 1, 'pdf': 1, 'struct.txt': 1, 'txt': 1}
+		self.assertEqual(fileChoices(['.xml'], self.aDir), {'xml': 1})
+	def checkRoot(self):
+		firstFunction = checkRoot()
+		firstFunction.self.mainDir()
+		assert frustration.status == 'changed correctly'
+		result = checkRoot(self.mainDir)
+		self.assertEqual(result, )
 	def tearDown(self):
-		self.dir.dispose()
-		self.dir = None
-
-
-# def suite():
-# 	tests = ['xTestCase']
-# 	return unittest.TestSuite(map(ProjTestCase))
+		shutil.rmtree(self.mainDir)
 
 if __name__ == '__main__':
 	unittest.main()
